05c14a69f47f8260772a4b3977d0d01b4547136f
Rebaone27 <moipolairebaone@gmail.com>
2022-11-09 21:32:42 +0200

    Updated code to take in user input and print it out.

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class helloWorld {
     public void main(String[] args) {
-        System.out.println("Git is Awesome!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How do you feel about version control?");
+        String answer = input.next();
+        System.out.println(answer);
     }
 }
